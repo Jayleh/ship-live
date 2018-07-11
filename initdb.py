ab84a4e113879ac81fc4ff37f2c3b30ca30b9c05
Jayleh <jaylehyun@gmail.com>
2018-07-11 16:23:31 -0700

    troubleshooting database

--- initdb.py
+++ initdb.py
@@ -1,4 +1,5 @@
 from shipstation.models import db
+import os
 
 # db.drop_all()
 db.create_all()
--- shipstation/__init__.py
+++ shipstation/__init__.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.sqlite"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', '') or "sqlite:///site.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
