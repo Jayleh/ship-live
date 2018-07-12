74174360916f5c218aac02f735bf0006049440cd
Jayleh <jaylehyun@gmail.com>
2018-07-11 22:17:11 -0700

    adding debug statements

--- shipstation/routes.py
+++ shipstation/routes.py
@@ -33,6 +33,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+        print(user)
+        print(form.password.data)
+        print(bcrypt.check_password_hash(user.password, form.password.data))
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get("next")
